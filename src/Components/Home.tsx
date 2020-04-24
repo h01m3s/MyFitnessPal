2eab65376752be3c9521df5a7f351e8d33992b9e
h01m3s <lin426797@gmail.com>
2020-04-24 15:13:30 -0400

    Integrate components with Home Page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,18 +1,36 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Auth';
-import { signOut } from '../API/firebase';
+import UserCard from './UserCard';
+import Hero from './Hero';
+import EventList from './EventList';
+import UserList from './UserList';
 
 const Home: React.FC = () => {
   const currentUser = useContext(AuthContext);
 
   return (
-    <>
-      <h1>Home Page</h1>
-      <h2>User: {currentUser ? currentUser.email : ''}</h2>
-      <button className="button is-danger" onClick={() => signOut()}>
-        Log out
-      </button>
-    </>
+    <div className="container">
+      <Hero />
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <UserCard
+            user={{
+              name: 'John Appleseed',
+              id: 'johnapple',
+              friends: [
+                { name: 'John Appleseed', id: 'johnapple', friends: [] }
+              ]
+            }}
+          />
+        </div>
+        <div className="column is-half" style={{ background: 'white' }}>
+          <EventList />
+        </div>
+        <div className="column">
+          <UserList />
+        </div>
+      </div>
+    </div>
   );
 };
 export default Home;
